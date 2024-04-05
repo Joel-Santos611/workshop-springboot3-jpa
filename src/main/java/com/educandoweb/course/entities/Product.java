2207a9fb3118881ca8cb158d36e42038c6571239
Joel-santos611 <joeleduardodossantos611@gmail.com>
2024-04-05 12:57:42 +0100

    update

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -20,7 +21,8 @@ private String description;
 private Double price;
 private String imgUrl;
 
-@Transient
+@ManyToMany
+@
 private Set<Category> categories = new HashSet<>();
 
 public Product() {
--- src/main/java/com/educandoweb/course/services/ProductService.java
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -6,9 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Product;
-import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.ProductRepository;
 
 @Service
